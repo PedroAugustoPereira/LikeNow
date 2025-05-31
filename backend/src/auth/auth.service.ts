9cb5d996a064a437aaafd19916922768a8190f22
Matheus de Oliveira Kur <matheuskur7@gmail.com>
2025-05-31 14:40:22 -0300

    cors

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,17 +6,18 @@ import {
 } from '@nestjs/common';
 import { PrismaService } from './../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
-import { AuthEntity } from './entities/AuthEntity';
 import * as bcrypt from 'bcrypt';
-import { log } from 'console';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(email: string, password: string){
+  async login(email: string, password: string) {
     console.log('Attempting login for email:', email);
-    
+
     const user = await this.prisma.user.findUnique({ where: { email: email } });
 
     if (!user) {
