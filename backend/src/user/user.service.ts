17c5841784cb85c8b60a34e599fbcddb013e6801
Matheus de Oliveira Kur <matheuskur7@gmail.com>
2025-05-31 02:12:49 -0300

    service Prisma created

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,19 +1,25 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
